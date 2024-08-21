5f8dc2700b6435238fc3d7a94a72d470faa24e02
Swarup-Dash <swarupdash7205@gmail.com>
2024-08-21 16:56:24 +0530

    Initial Commit

---
+++ removeDuplicate.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeDuplicate(self, arr):
+        start = 0
+        
+        while start<len(arr)-1:
+            if arr[start] == arr[start+1]:
+                arr.pop(start+1)                
+            else:
+                start+=1
+       
+        return arr
+ 
+obj = Solution()
+arr = [4,4,6,6,8]
+res = obj.removeDuplicate(arr)
+print(res)
