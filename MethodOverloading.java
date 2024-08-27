a85d05e50c3ff3fb87bcae6b7f46f8516c69b774
Swarup-Dash <swarupdash7205@gmail.com>
2024-08-27 09:32:13 +0530

    initial Commit

---
+++ MethodOverloading.java
@@ -0,0 +1,17 @@
+class A{
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public double add(double a, double b){
+        return a+b;
+    }
+}
+
+public class MethodOverloading {
+    public static void main(String[] args) {
+        A obj = new A();
+        System.out.println(obj.add(5, 10));
+        System.out.println(obj.add(15.5, 4.5));
+    }
+}
---
+++ MethodOverriding.java
@@ -0,0 +1,32 @@
+class A {
+    public void name() {
+        System.out.println("My name is Swarup");
+    }
+}
+
+class B extends A {
+    @Override
+    public void name() {
+        System.out.println("My name is Arnab");
+    }
+}
+
+class C extends A {
+    @Override
+    public void name() {
+        System.out.println("My name is Mukesh");
+    }
+}
+
+public class MethodOverriding {
+    public static void main(String[] args) {
+
+        A objA = new A();
+        A objB = new B();
+        A objC = new C();
+
+        objA.name();  // Calls A's name method
+        objB.name();  // Calls B's overridden name method
+        objC.name();  // Calls C's overridden name method
+    }
+}
