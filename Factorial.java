9b29147f0dc9470ddabf3909452092744b9972bc
Swarup-Dash <swarupdash7205@gmail.com>
2024-09-19 06:32:15 +0530

    Initial Commit

--- Factorial.java
+++ Factorial.java
@@ -1,12 +1,12 @@
 public class Factorial {
     public static void main(String[] args) {
         int k = 10;
-        Factorial fact = new Factorial();
-        int res = fact.factorials(k);
+        // Factorial fact = new Factorial();
+        int res = factorials(k);
         System.out.println(res);  
     }
     
-    public int factorials(int k) {
+    public static int factorials(int k) {
         if (k > 0) {
             return k * factorials(k - 1);
         } else {
