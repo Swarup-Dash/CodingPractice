61901a197ea3736c34a9165720d793893e6e2819
Swarup Dash <swarupdash7205@gmail.com>
2025-02-03 22:24:17 +0530

    commit

---
+++ FindSubArray.java
@@ -0,0 +1,17 @@
+public class FindSubArray{
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        findSubarray(arr);
+    }
+    public static void findSubarray(int[] arr){
+        for(int i=0; i< arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                System.out.print("[ ");
+                for(int k=i;k<=j;k++){
+                    System.out.print(arr[k]+" ");
+                }
+                System.out.print("]");
+            }
+        }
+    }
+}
