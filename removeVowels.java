c79dd49dfcf3cd70926b45fe912bb40e9009e220
Swarup Dash <swarupdash7205@gmail.com>
2025-02-18 22:21:39 +0530

    commit

---
+++ removeVowels.java
@@ -0,0 +1,13 @@
+public class removeVowels {
+    public static void main(String[] args) {
+       String str = "Remove all Vowels";
+       String res = "";
+       char[] ch = str.toCharArray();
+       for(char c:ch){
+        if(c!='a' && c!='e' && c!='i' && c!='o' && c!='u' && c!='A' && c!='E' && c!='I' && c!='O' && c!='U'){
+            res+=c;
+        }
+       }
+       System.out.println(res); 
+    }
+}
